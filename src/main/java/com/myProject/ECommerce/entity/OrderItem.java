75938e4a6aa6382cb13f794bbc98a3331f4c74dc
kartikay <kartikayshukla3305@gmail.com>
2024-08-09 18:07:08 +0530

    update orders

--- src/main/java/com/myProject/ECommerce/entity/OrderItem.java
+++ src/main/java/com/myProject/ECommerce/entity/OrderItem.java
@@ -1,8 +1,33 @@
 package com.myProject.ECommerce.entity;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 @Entity
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @JsonIgnore
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+    private String size;
+    private int quantity;
+    private Integer price;
+
+    private Integer discountedPrice;
+    private Long userId;
+    private LocalDateTime deliveryDate;
 
 }
