47b40b88dcdeffd48af9b21a65f12b4b50a7d8b7
kartikay <kartikayshukla3305@gmail.com>
2024-08-19 21:22:33 +0530

    controllers updated

--- src/main/java/com/myProject/ECommerce/controller/OrderController.java
+++ src/main/java/com/myProject/ECommerce/controller/OrderController.java
@@ -1,4 +1,48 @@
 package com.myProject.ECommerce.controller;
 
+import com.myProject.ECommerce.entity.Address;
+import com.myProject.ECommerce.entity.Order;
+import com.myProject.ECommerce.entity.User;
+import com.myProject.ECommerce.exception.OrderException;
+import com.myProject.ECommerce.exception.UserException;
+import com.myProject.ECommerce.service.OrderService;
+import com.myProject.ECommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/orders")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<Order> create(@RequestBody Address shippingAddress, @RequestHeader("Authorization")String jwt) throws UserException{
+        User user = userService.findUserProfileByJwt(jwt);
+        Order order = orderService.createOrder(user, shippingAddress);
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<List<Order>> userOrderHistory(@RequestHeader("Authorization")String jwt) throws UserException{
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Order> orders = orderService.userOrderHistory(user.getId());
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> findOrderById(@PathVariable("orderId")Long orderId, @RequestHeader("Authorization")String jwt) throws UserException, OrderException{
+        User user = userService.findUserProfileByJwt(jwt);
+        Order order = orderService.findOrderById(orderId);
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/myProject/ECommerce/controller/ProductController.java
+++ src/main/java/com/myProject/ECommerce/controller/ProductController.java
@@ -1,4 +1,37 @@
 package com.myProject.ECommerce.controller;
 
+import com.myProject.ECommerce.entity.Product;
+import com.myProject.ECommerce.exception.ProductException;
+import com.myProject.ECommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<Page<Product>> findProductByCategoryHandler(@RequestParam String category, @RequestParam List<String> color,
+                                                                                             @RequestParam List<String> size, @RequestParam Integer minPrice,
+                                                                                             @RequestParam Integer maxPrice, @RequestParam Integer mixDiscount,
+                                                                                             @RequestParam String sort, @RequestParam String stock,
+                                                                                             @RequestParam Integer pageNumber, @RequestParam Integer pageSize){
+        Page<Product> res = productService.getAllProducts(category,color,size,minPrice,maxPrice,mixDiscount,sort,stock,pageNumber,pageSize);
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/products/id/{productId")
+    public ResponseEntity<Product> findProductByIdHandler(@PathVariable Long productId) throws ProductException{
+        Product product = productService.findProductById(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
 }
